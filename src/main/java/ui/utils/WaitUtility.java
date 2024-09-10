2dfbfe4d126cbf7e8ef7ecd58b29a7bc4cc963a3
Ganeshaprasad <ganesha.prasad1996@gmail.com>
2024-09-10 15:03:21 +0530

    modified_framework

--- src/main/java/ui/utils/WaitUtility.java
+++ src/main/java/ui/utils/WaitUtility.java
@@ -1,4 +1,19 @@
 package ui.utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class WaitUtility {
+
+    private WebDriverWait webDriverWait;
+
+    public boolean waitForVisibilityOfElement(WebDriver driver, int timeInSeconds, WebElement element)
+    {
+        webDriverWait=new WebDriverWait(driver, Duration.ofSeconds(timeInSeconds));
+        return webDriverWait.until(ExpectedConditions.visibilityOf(element)).isDisplayed();
+    }
 }
--- src/test/java/ui_test/BaseTest.java
+++ src/test/java/ui_test/BaseTest.java
@@ -1,28 +1,46 @@
 package ui_test;
 
+
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import ui.utils.GeneralUtility;
+import org.testng.annotations.Parameters;
+import ui.utils.*;
 
 import java.io.IOException;
 
-public class BaseTest {
+public class BaseTest
+{
 
     public static WebDriver driver;
-    public GeneralUtility generalUtility= new GeneralUtility();
+   private FileUtility fileUtility;
+
+   public static Logger logger;
+
+    @BeforeMethod(alwaysRun = true)
+    @Parameters("baseurl")
+    public void setUp(String baseurl) throws IOException {
+
+        logger=Logger.getLogger("Java-Selenium-Api-Hybrid");
+        PropertyConfigurator.configure("log4j.properties");
 
 
-    @BeforeMethod
-    public void setUp() throws IOException {
-        if (generalUtility.readDataFromPropertyFile("config.properties", "browser").equals("chrome")) {
+        fileUtility= new FileUtility();
+        if (fileUtility.readDataFromPropertyFile("config.properties", "browser").equals("chrome")) {
             driver = new ChromeDriver();
-        } else if (generalUtility.readDataFromPropertyFile("config.properties", "browser").equals("firefox")) {
+            logger.info("Chrome browser launched");
+        } else if (fileUtility.readDataFromPropertyFile("config.properties", "browser").equals("firefox")) {
             driver=new FirefoxDriver();
+            logger.info("Firefox browser launched");
         }
-        driver.get("https://www.shutterfly.com/");
+        ScreenShotUtility.setDriver(driver);
+
+        driver.get(baseurl);
     }
 
     @AfterMethod
